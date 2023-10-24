686e310603e243a77bf94ff0d2559b7203e5ee60
phajw <dscr@duck.com>
2023-10-24 10:00:23 -0400

    Finish login form, remove coverage reports

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,8 @@
 import { FontAwesome5 } from '@expo/vector-icons';
-import { Button, Image, Input } from '@rneui/base';
+import { Button, Image } from '@rneui/base';
 import { Formik } from 'formik';
-import { useState } from 'react';
-import { Text, View, Pressable, KeyboardAvoidingView } from 'react-native';
+import { Text, View, Pressable, KeyboardAvoidingView, TextInput } from 'react-native';
+
 import tw from 'twrnc';
 import * as yup from 'yup';
 
@@ -18,60 +18,77 @@ import * as yup from 'yup';
  * @returns {JSX.Element} The login screen component.
  *
  */
+
 export default function LoginScreen({ navigation }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const validationSchema = yup.object().shape({
-    email: yup.string().email('Please enter a valid email').required('Email Address is required'),
+    email: yup.string().email('Please enter a valid email.').required('Email Address is required.'),
+    password: yup.string().min(6).max(24).required('Minimum 6 characters required.'),
   });
   return (
     <Formik
-      initialValues={{}}
+      initialValues={{ email: '', password: '' }}
       validationSchema={validationSchema}
       onSubmit={(values) => {
         console.log(values);
       }}>
-      {({ handleSubmit, errors }) => (
-        <KeyboardAvoidingView style={tw`items-center flex-1 justify-center p-8`}>
+      {({ values, handleSubmit, handleChange, isValid, errors }) => (
+        <KeyboardAvoidingView style={tw`flex justify-center items-center p-8`}>
           <Image
-            style={tw`w-70 h-70 p-2 pb-4`}
+            style={tw`w-70 h-70 p-8 pb-4`}
             source={{
               uri: 'https://raw.githubusercontent.com/dsrcr/stellar-message/master/assets/stellarnet_logo.svg?token=GHSAT0AAAAAACJJPOLD6WT5V6YXE2RJP4Y6ZJW2STA',
             }}
           />
-          <Input
-            required
+          <TextInput
+            autoFocus
+            id="email"
+            inputMode="email"
+            style={tw`w-full border-2 border-gray-300 rounded-xl p-4 mt-8`}
+            placeholder="E-mail address"
+            placeholderTextColor="gray"
+            value={values.email}
+            onChangeText={handleChange('email')}
             keyboardType="email-address"
-            value={email}
-            onChange={(event) => {
-              setEmail(event.target.value);
-            }}
-            style={tw`pt-8`}
-            autoComplete="email"
-            placeholder="E-mail Address"
+            textContentType="emailAddress"
+            name="email"
           />
-          <Input
+          {errors.email ? <Text style={tw`text-red-500 mt-4`}>{errors.email}</Text> : null}
+          <TextInput
             id="password"
-            name="password"
-            style={tw`pt-4`}
-            secureTextEntry
+            inputMode="text"
+            value={values.password}
+            onChangeText={handleChange('password')}
+            style={tw`w-full border-2 border-gray-300 rounded-xl p-4 mt-8`}
             placeholder="Password"
+            placeholderTextColor="gray"
+            secureTextEntry
+            textContentType="password"
+            name="password"
           />
 
+          {errors.password ? <Text style={tw`text-red-500 mt-4`}>{errors.password}</Text> : null}
           <Pressable style={tw`p-4`}>
             <Text>Forgot password?</Text>
           </Pressable>
 
-          <Button onPress={handleSubmit} title="Login" containerStyle={tw`m-4 p-4 w-80`} />
+          <Button
+            disabled={!isValid}
+            size="lg"
+            onPress={handleSubmit}
+            title="Login"
+            containerStyle={tw`w-full p-4`}
+          />
 
           <Text>or use</Text>
-          <View style={tw`flex-row items-center p-2`}>
+          <View style={tw`flex justify-center items-center flex-row`}>
             <Button
-              containerStyle={tw`m-4 p-4 w-40`}
+              size="lg"
+              containerStyle={tw`p-4 min-w-41`}
               title={<FontAwesome5 name="google" color="white" size={24} />}
             />
             <Button
-              containerStyle={tw`m-4 p-4 w-40`}
+              size="lg"
+              containerStyle={tw`p-4 min-w-41`}
               title={<FontAwesome5 name="apple" color="white" size={24} />}
             />
           </View>
