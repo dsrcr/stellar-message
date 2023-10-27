0b235d8389743ac06738a0278f1d7e2c49c3e21d
phajw <dscr@duck.com>
2023-10-28 00:59:29 +0200

    Fix register unit test warnings

--- __tests__/Register.test.js
+++ __tests__/Register.test.js
@@ -3,6 +3,18 @@ import renderer from 'react-test-renderer';
 
 import RegisterScreen from '../screens/RegisterScreen';
 
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+}));
+
 describe('<RegisterScreen />', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<RegisterScreen />).toJSON();
